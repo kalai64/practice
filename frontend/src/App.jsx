b58814e1b0928faa235cd4e66591ac478e716fa3
kalai64 <kalaikabil6464@gmail.com>
2026-03-30 19:09:17 +0530

    Fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
+
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/create' element={<Create/>}/>
         <Route path='/update/:id' element={<Update/>}/>
       </Routes>
+      
     </BrowserRouter>
     </>
   )
